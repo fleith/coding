09224c3f135a395156cbbf2b4b55e489bc76ed3f
Álvaro Fleith <alvarofleith@gmail.com>
2017-07-04 13:58:54 -0300

    check find_single solution on py.test

--- single_test.py
+++ single_test.py
@@ -6,5 +6,9 @@ def find_single(elements):
         single = single ^ element
     return single
 
-print(find_single([1,3,1,7,9,3,9]))
-print(find_single([1,3,1,7,9,3,9,8,8]))
+def test_single_element():
+    assert find_single([1,3,1,7,9,3,9]) == 7
+    assert find_single([1,3,1,7,9,3,9,8,8]) == 7
+    assert find_single([1,1,1,1,1,1]) == 0
+    assert find_single([1,1,1,1,1,1,1]) == 1
+    assert find_single([1,2,3,4,5,6,7,8,9,10,1,2,3,5,6,7,8,9,10]) == 4
