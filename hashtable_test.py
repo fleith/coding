74f9f61cc10d42f0f1a6fbb44510a6beb739e003
Alvaro <alvarofleith@gmail.com>
2017-09-15 16:29:05 -0300

    Change dict to list

--- hashtable_test.py
+++ hashtable_test.py
@@ -1,14 +1,33 @@
 class HashMap(object):
-    def __init__(self):
-        self.mydict = dict()
-    def __setitem__(self, key, value):
-        self.mydict[key] = value
-    def __getitem__(self, key):
-        return self.mydict[key]
 
-myd = dict
+    def __init__(self, size=None, hash_function=None):
+        assert isinstance(size, int)
+        self.array = list()
+        for x in range(size):
+            self.array.append(None)
+        self.size = size
+        self.hash_function = hash_function
+
+    def __setitem__(self, key, value):
+        self.array[hash(key) % self.size] = value
+
+    def __getitem__(self, key):
+        return self.array[hash(key) % self.size]
+
+    def dump(self):
+        for x in self.array:
+            print(x)
+
+
 
 def test_hashmap():
-    hm = HashMap()
+    hm = HashMap(10)
     hm['a'] = 10
     assert hm['a'] == 10
+    hm['a'] = 11
+    assert hm['a'] == 11
+    hm['b'] = 32
+    assert hm['b'] == 32
+    hm[2] = 3343
+    assert hm[2] == 3343
+    hm.dump()
