772c914b92a2d4e6a9fcad73b7191c73dc4f6f67
Alvaro <alvarofleith@gmail.com>
2017-09-21 16:15:50 -0300

    test hashmap balanced binary tree, iterate over all elements (next step I will use binary search)

--- hashtable_balanced_binary_tree_test.py
+++ hashtable_balanced_binary_tree_test.py
@@ -5,7 +5,12 @@ class HashMap(object):
         self.hash_function = hash_function
 
     def __setitem__(self, key, value):
-        key_list = self.tree[self.hash_function(key)]
+        hash_value = self.hash_function(key)
+        key_list = [x[1] for x in self.tree if x[0] == hash_value]
+        if not key_list:
+            key_list = list()
+            self.tree.append((hash_value, key_list))
+            self.tree.sort(key=lambda key: key[0])
         for index,item in enumerate(key_list):
             if item[0] == key:
                 key_list[index] = key, value
@@ -13,8 +18,9 @@ class HashMap(object):
         key_list.append((key, value))
 
     def __getitem__(self, key):
-        key_list = self.tree[self.hash_function(key)]
-        for item in key_list:
+        hash_value = self.hash_function(key)
+        key_list = [x[1] for x in self.tree if x[0] == hash_value]
+        for item in key_list[0]:
             if item[0] == key:
                 return item[1]
 
